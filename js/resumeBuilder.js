dea3d8e384d330af9849330fe177d856b9aacb80
FBieg <frankely019@gmail.com>
2017-09-15 15:52:44 -0400

    Part One

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var skills = ["ladron","traficante","pelotero","programador"]
+
+//$("#main").append(skills.length)
+
+var bio = {
+    "name":"Christian",
+    "role": "Developer",
+    "contactinfo":8092998585,
+    "urlpic":"images/me.jpg",
+    "welcomemsg":"Hi there! I'm using this.",
+    "skills":skills
+}
+for(key in bio){
+    //console.log(key);
+    $("#main").append(bio[key]);
+    $("#main").append("<br>");
+}
